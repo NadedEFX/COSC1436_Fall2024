501646b1c544a423f47f7ca0bf285fbeded58463
NadedEFX <connersantiago@gmail.com>
2024-11-20 05:03:51 +0000

    commit

--- Week_13_Vehicle_Project/Car.java
+++ Week_13_Vehicle_Project/Car.java
@@ -1,18 +1,35 @@
+
+
 public class Car extends Vehicle {
     // Private attribute for the number of doors that can't be used outside of Car class
     private int numberOfDoors;
+    private boolean hasEngine;
 
     // Constuctor for the class
-    public Car(int speed, double fuel, int numberOfDoors) {
+    public Car(int speed, double fuel, int numberOfDoors, boolean hasEngine) {
         super(speed, fuel); //Calls the Vehicle constructor
         this.numberOfDoors = numberOfDoors;
+        this.hasEngine = hasEngine;
     }
+
     // Override the start method from the Vehicle class to display a new message
     @Override
-    // Method to start the vehicle 
+    // Method to start the vehicle also additional steps to have the car have an engine or not
     public void start(){
-        super.start();
-        System.out.println("Car has started");
+        // If shows that if it does have the engine it'll let you start up the car no problem
+        if(hasEngine){
+            super.start(); // lets you call the Vehicle constructor
+            System.out.println("Car has started: Let's get this puppy on the rode");
+        }
+        // Else incase the car doesn't have an engine doesn't turn on like a real car if it doesn't have one
+            else{
+                stop();
+                System.out.println("No engine red neck response: I know what's wrong with it, it got no gas in it");
+            }
+    }
+    // Method that makes sure the car doesn't turn on if there isn't an engine
+    public void stop(){
+        System.out.println("Car doesn't start");
     }
     // Method to make the car be able to honk
     public void honk(){
@@ -25,5 +42,14 @@ public class Car extends Vehicle {
     // setter for number of doors
     public void setNumberOfDoors(int numberOfDoors){
         this.numberOfDoors = numberOfDoors;
+    }
+    //setter for the Engine
+    public boolean hasEngine(){
+        return hasEngine;
+    }
+    // getter for the Engine
+    public void setHasEngine(boolean hasEngine){
+        this.hasEngine = hasEngine;
+    
     }
 }
--- Week_13_Vehicle_Project/Week13Runner.java
+++ Week_13_Vehicle_Project/Week13Runner.java
@@ -2,25 +2,48 @@ public class Week13Runner
 {
     public static void main(String [] args)
     {
-        Car myCar = new Car(120,90,4);
-        System.out.println("Testing the car");
+        //Car object
+        Car myCar = new Car(120,90,4,true);
+        System.out.println("Testing the car that has an engine");
+        // uses the start up command in Vehicle but is overriden by the Car start Method
         myCar.start();
+        //shows the fuel of the car
+        myCar.displayFuel();
+        // makes the car honk by grabbing the Method from the Car Class
+        myCar.honk();
+        // using ther setter in the car class to change the amount of fuel the car has
+        myCar.setFuel(20);
+        // using the setter to change the number of doors the car has
+        myCar.setNumberOfDoors(2);
+        // displaying the new amount of fuel now because the setter changed the getter
+        myCar.displayFuel();
+
+        System.out.println("\nTesting the car without the engine");
+        // changed the setter of the boolean to false so it gives the idea of a car without an engine
+        myCar.setHasEngine(false);
+        // Calls the start Method but it triggers the else because there's no engine in the car
+        myCar.start();
+        // displays the 
         myCar.displayFuel();
         myCar.honk();
 
-        myCar.setFuel(20);
-        System.out.println("Fuel level now at 4" + myCar.getFuel());
-        myCar.setNumberOfDoors(2);
-
-
+        // Bicycle object
         Bicycle myBicycle = new Bicycle(25,0,true);
         System.out.println("\nTesting the bicycle");
+        // using the start up command in Vehicle but is overriden by the Bicycle start Method
         myBicycle.start();
+        // shows the amount fuel you have
         myBicycle.displayFuel();
+        // allows the bike to use it's bell by getting the Method from the Bicycle class
         myBicycle.hasBell();
         
+        /* changing the set amount of fuel to 0 because a bicycle doesn't 
+        run on fuel the setter and getter run together the setter gets the
+         new infomation and sends it to the getter then the getter sends it
+          to the constructor to change the value */  
         myBicycle.setFuel(0);
         System.out.println("Bicycle doesn't need fuel because it's a bike");
+        // changing the boolean of the bell to false to give the bike no bell, it uses the setter to accomplish this command
         myBicycle.setHasBell(false);
         myBicycle.hasBell();
     }
